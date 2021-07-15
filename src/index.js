aea636cbd06d1ef9325d86ad64156107dcb8b5c0
will123195 <will123195@gmail.com>
2021-07-15 16:05:01 -0600

    parameterize query

--- src/index.js
+++ src/index.js
@@ -22,9 +22,9 @@ export default class GraphSync {
     }
   }
 
-  async pgQuery(sql) {
+  async pgQuery(sql, values) {
     const client = await this.pgPool.connect()
-    const result = await client.query(sql)
+    const result = await client.query(sql, values)
     client.release()
     return result
   }
@@ -33,9 +33,9 @@ export default class GraphSync {
     const { rows } = await this.pgQuery(`
       SELECT conname, pg_catalog.pg_get_constraintdef(r.oid, true) as condef
       FROM pg_catalog.pg_constraint r
-      WHERE r.conrelid = (SELECT oid FROM pg_class WHERE relname = '${tableName}')
+      WHERE r.conrelid = (SELECT oid FROM pg_class WHERE relname = $1)
       AND r.contype = 'f'
-    `)
+    `, [tableName])
     const foreignKeys = {}
     rows.forEach(r => {
       var regExp = /\(([^)]+)\) REFERENCES (.+)\(([^)]+)\)/
