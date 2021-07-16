1d26db2c8bc008084cd106ba4a418d1a7be6f260
will123195 <will123195@gmail.com>
2021-07-16 13:22:25 -0600

    add more tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -7,23 +7,26 @@ import fs from 'fs'
 
 let pgPool
 let graphSync
+
 const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
+const findOne = async (tableName, id) => {
+  const { rows } = await graphSync.pgQuery(`select * from "${tableName}" where id = $1`, [id])
+  return rows[0]
+}
 
 test.before(async function () {
-  // this.timeout(120*1000);
   await pgSetup()
-  //set timeout to make sure the in-memory-db is already setup.
-  await wait(5000)
+  await wait(1000)
   pgPool = new pg.Pool({ connectionString: process.env.DATABASE_URL })
   graphSync = new GraphSync({ pgPool })
   const sql = fs.readFileSync(`${__dirname}/fixtures.sql`, 'utf8')
   await graphSync.pgQuery(sql)
 })
 
-test.serial('register tables', async t => {
+test.serial('registerTable', async t => {
   await graphSync.registerTable({
     tableName: 'authors',
-    getLabels: () => ['Author'],
+    getLabels: () => ['Person', 'Author'],
     getProperties: row => row
   })
   await graphSync.registerTable({
@@ -32,17 +35,27 @@ test.serial('register tables', async t => {
     getProperties: row => ({ id: row.id, title: row.title })
   })
   t.is(graphSync.tables.books.foreignKeys.author.foreignTable, 'authors')
-  t.is(graphSync.tables.books.getLabels()[0], 'Book')
+  t.deepEqual(graphSync.tables.books.getLabels(), ['Book'])
 })
 
-test.serial('generate node', async t => {
-  const { rows } = await graphSync.pgQuery('select * from books where id = $1', [1])
+test.serial('generateNode single label', async t => {
+  const row = await findOne('books', 1)
   const cypher = await graphSync.generateNode({
     tableName: 'books',
-    row: rows[0]
+    row
   })
   t.is(cypher, "MERGE (:Book {id: 1, title: 'The Great Gatsby'});")
 })
+
+test.serial('generateNode multiple labels', async t => {
+  const row = await findOne('authors', 1)
+  const cypher = await graphSync.generateNode({
+    tableName: 'authors',
+    row
+  })
+  t.is(cypher, "MERGE (:Person:Author {id: 1, name: 'F. Scott Fitzgerald'});")
+})
+
 
 test.after(() => {
   pgTeardown()
